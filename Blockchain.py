52c49fe1512d343abfbca758458ce5f5059b577d
Paul Wiper <you@example.com>
2019-06-09 20:15:21 +0200

    Added new_block functionality for genisis block aswell as hashing

--- Blockchain.py
+++ Blockchain.py
@@ -2,15 +2,38 @@
 ---- Blockchain with Python, written by Paul Wiper ----
 ---- created following HackerNoon guide written by Daniel van Flymen ----
 """
-#Blockchain classes constructors create an empty list to store blockchain (BC) and another to store transactions
-
+#Blockchain classes constructors create an empty list to store blockchain (BC)
+#and another to store transactions
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
-        def new_block(self):
-            pass
+        #Create the genisis (first) block
+        self.new_block(previous_hash=1, proof=100)
+
+        def new_block(self, proof, previous_hash=None):
+            """
+            Creates new Block in BC
+
+            :param proof: <int> The proof given by the Proof of Work Alg
+            :param previous hash: (optional) <str> hash of the prev block
+            :return: <dict> New block
+            """
+
+            block = {
+                'index': len(self.chain) + 1,
+                'timestamp': time(),
+                'transactions': self.current_transactions,
+                'proof': proof,
+                'previous_hash': previous_hash or self.hash(self.chain[-1]),
+                }
+
+            # Clear the current transaction list
+            self.current_transactions = []
+
+            self.chain.append(block)
+            return block
 
         def new_transaction(self):
             """
@@ -29,16 +52,23 @@ class Blockchain(object):
                 })
 
             return self.last_block['index'] + 1
-            # return index for the block this trans info will be appended to by incrementing last blocks index
+            # return index for the block this trans info will be appended to by
+            # incrementing last blocks index
 
         @staticmethod
         def hash(block):
-            # Hashes a block
+            """ Hashes a block using SHA-256
+                :param block: <dict> Block
+                :return: <str>
+            """
+
+            # Must make sure Dictionary is ordered or hashes will be inconsistent
+            block_string = json.dumps(block, sort_keys=True).encode()
+            return hashlib.sha256(block_string).hexdigest()
+
             pass
         
         @property
         def last_block(self):
             #returns last block in chain
             pass
-
-        5UrJ0vaez5C1s
